b204c3e1c002171d55fa85a758bf0be14c3229e6
ShizaIncorp <150190398+ShizaIncorp@users.noreply.github.com>
2024-10-19 22:12:03 +0300

    Update Task2.py

--- Лабораторная 1/Task2.py
+++ Лабораторная 1/Task2.py
@@ -1,11 +1,11 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
 
-info_vol = 1.44*1024*1024
+info_vol = 1.44 * 1024 * 1024
 total_page = 100
 numbers_str = 50
 total_sym = 25
 
-one_book_memory = 4*total_sym*numbers_str*total_page
-total_book = round(info_vol//one_book_memory, )
+one_book_memory = 4 * total_sym * numbers_str * total_page
+total_book = info_vol // one_book_memory
 
-print("Количество книг, помещающихся на дискету:", total_book)
+print("Количество книг, помещающихся на дискету:", int(total_book))
