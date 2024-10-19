b082bc865e994fa9b2abf6998f0851e08fa6d3cb
ShizaIncorp <150190398+ShizaIncorp@users.noreply.github.com>
2024-10-19 22:12:45 +0300

    Update Task1.py

--- Лабораторная 1/Task1.py
+++ Лабораторная 1/Task1.py
@@ -4,6 +4,6 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 numbers[4] = 0
 
-numbers[4] = sum(numbers)/len(numbers)
+numbers[4] = sum(numbers) / len(numbers)
 
 print("Измененный список:", numbers)
