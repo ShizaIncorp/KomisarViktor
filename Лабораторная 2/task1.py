a241f005e030dd222d80ead06dc4d227c6a0bd5e
ShizaIncorp <150190398+ShizaIncorp@users.noreply.github.com>
2024-10-29 22:15:02 +0300

    Update task1.py

--- Лабораторная 2/task1.py
+++ Лабораторная 2/task1.py
@@ -3,14 +3,14 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
 
-total_money = money_capital + salary
+begin_month_money = money_capital + salary
 
 month = 0
 
-while total_money > spend:
-    total_money = total_money - spend
-    total_money += salary
-    spend = (1 + increase) * spend
+while begin_month_money >= spend:
+    begin_month_money = begin_month_money - spend
+    begin_month_money += salary
+    spend = (1+increase) * spend
     month += 1
 
 print("Количество месяцев, которое можно протянуть без долгов:", month)
