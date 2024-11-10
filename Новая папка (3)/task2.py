504dbba7971da9745b6120cb3b88485ab4e0dfed
ShizaIncorp <150190398+ShizaIncorp@users.noreply.github.com>
2024-11-10 20:21:43 +0300

    Add files via upload

---
+++ Новая папка (3)/task2.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+
+# TODO решите задачу
+def task() -> float:
+    a = os.listdir()
+    way_json_obj = os.path.join(os.getcwd(), a[0])
+    with open(way_json_obj) as f:
+        pyton_obj = json.load(f)
+
+    save_number = []
+    for dict in pyton_obj:
+        comp = dict["score"] * dict["weight"]
+        save_number.append(comp)
+    return sum(save_number)
+
+
+print(f"{task():.3f}")
