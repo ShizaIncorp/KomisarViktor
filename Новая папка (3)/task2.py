1276333ca3eac0d13b740ac3d254bbf7b87ce299
ShizaIncorp <150190398+ShizaIncorp@users.noreply.github.com>
2024-11-13 16:01:16 +0300

    Update task2.py

--- Новая папка (3)/task2.py
+++ Новая папка (3)/task2.py
@@ -1,19 +1,23 @@
-import os
 import json
+import csv
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
 
 
-# TODO решите задачу
-def task() -> float:
-    a = os.listdir()
-    way_json_obj = os.path.join(os.getcwd(), a[0])
-    with open(way_json_obj) as f:
-        pyton_obj = json.load(f)
+def task() -> None:
+    with open(INPUT_FILENAME) as f:
+        lines = [line for line in csv.DictReader(f)]
 
-    save_number = []
-    for dict in pyton_obj:
-        comp = dict["score"] * dict["weight"]
-        save_number.append(comp)
-    return sum(save_number)
+    with open(OUTPUT_FILENAME, 'w') as output_f:
+        json.dump(lines, output_f, indent=4)
+
+    with open(OUTPUT_FILENAME, 'r') as output_f:
+        for line in output_f:
+            print(line, end="")
+    return None
 
 
-print(f"{task():.3f}")
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
